b8cf5e741c2ff795fe1eb71d63d8717f54a94d7e
“Shixuan <shixuan.zhang@pnnl.gov>
2025-03-19 22:12:19 -0700

    Modification for E3SMv3 atmosphere model application

--- models/eam-se/model_mod.f90
+++ models/eam-se/model_mod.f90
@@ -63,7 +63,7 @@ use  ensemble_manager_mod,  only : ensemble_type, get_my_num_vars, get_my_vars
 use distributed_state_mod,  only : get_state
 use   state_structure_mod,  only : add_domain, get_dart_vector_index, get_domain_size, &
                                    get_dim_name, get_kind_index, get_num_dims, &
-                                   get_num_variables, get_varid_from_kind, &
+                                   get_num_variables, get_varid_from_kind, get_varid_from_varname, &
                                    get_model_variable_indices, state_structure_info, get_short_name, &
                                    get_long_name, get_dim_lengths, get_variable_name
 use  netcdf_utilities_mod,  only : nc_get_variable, nc_get_variable_size, nc_create_file, &
@@ -77,13 +77,14 @@ use  netcdf_utilities_mod,  only : nc_get_variable, nc_get_variable_size, nc_cre
                                    nc_synchronize_file, nc_end_define_mode, &
                                    nc_begin_define_mode, nc_open_file_readonly, &
                                    nc_close_file, nc_variable_exists, nc_get_global_attribute, &
-                                   nc_get_dimension_size, nc_global_attribute_exists
-
+                                   nc_get_dimension_size
 use        chem_tables_mod, only : init_chem_tables, finalize_chem_tables, &
                                    get_molar_mass, get_volume_mixing_ratio
 use     default_model_mod,  only : adv_1step, nc_write_model_vars, &
                                    init_time => fail_init_time,    &
-                                   init_conditions => fail_init_conditions
+                                   init_conditions => fail_init_conditions, &
+                                   parse_variables_clamp, &
+                                   MAX_STATE_VARIABLE_FIELDS_CLAMP
 
 use    eam_common_code_mod, only : above_ramp_start, are_damping, build_eam_pressure_columns, build_heights, &
                                    eam_grid, cdebug_level, check_good_levels, cno_normalization_of_scale_heights, &
@@ -97,8 +98,7 @@ use    eam_common_code_mod, only : above_ramp_start, are_damping, build_eam_pres
 
 
 use eam_common_code_mod, only : nc_write_model_atts, grid_data, read_grid_info, &
-                                set_eam_variable_info, MAX_STATE_VARIABLES, &
-                                num_state_table_columns, MAX_PERT, &
+                                MAX_PERT, &
                                 shortest_time_between_assimilations, domain_id, &
                                 cuse_log_vertical_scale, &
                                 cno_normalization_of_scale_heights, &
@@ -121,15 +121,15 @@ private
 public :: static_init_model,                   &
           get_model_size,                      &
           get_state_meta_data,                 &
-          model_interpolate,                   & 
+          model_interpolate,                   &
           shortest_time_between_assimilations, &
           nc_write_model_atts,                 &
-          write_model_time,                    & 
+          write_model_time,                    &
           read_model_time,                     &
           end_model,                           &
-          pert_model_copies,                   & 
-          convert_vertical_obs,                & 
-          convert_vertical_state,              & 
+          pert_model_copies,                   &
+          convert_vertical_obs,                &
+          convert_vertical_state,              &
           get_close_obs,                       &
           get_close_state
 
@@ -188,15 +188,14 @@ logical :: no_normalization_of_scale_heights = .true.
 ! for no clamping, use the string 'NA'
 ! to have the assimilation change the variable use 'UPDATE', else 'NO_UPDATE'
 
-character(len=vtablenamelength) :: state_variables(MAX_STATE_VARIABLES * &
-                                                   num_state_table_columns ) = ' '
+character(len=vtablenamelength) :: state_variables(MAX_STATE_VARIABLE_FIELDS_CLAMP) = ' '
 
 namelist /model_nml/  &
    dry_mass_vertical_coordinate,        &
    precise_dry_mass_get_close,          &
    eam_template_filename,               &
    eam_phis_filename,                   &
-   eam_use_pgrid,                       & 
+   eam_use_pgrid,                       &
    homme_map_file,                      &
    cs_grid_file,                        &
    vertical_localization_coord,         &
@@ -204,7 +203,7 @@ namelist /model_nml/  &
    assimilation_period_days,            &
    assimilation_period_seconds,         &
    use_log_vertical_scale,              &
-   no_obs_assim_above_level,            & 
+   no_obs_assim_above_level,            &
    model_damping_ends_at_level,         &
    suppress_grid_info_in_output,        &
    custom_routine_to_generate_ensemble, &
@@ -338,7 +337,6 @@ cassimilation_period_days = assimilation_period_days
 cassimilation_period_seconds = assimilation_period_seconds
 csuppress_grid_info_in_output = suppress_grid_info_in_output
 
-
 call set_calendar_type('GREGORIAN')
 
 call read_grid_info(eam_template_filename, eam_use_pgrid)
@@ -348,9 +346,12 @@ call read_eam_phis_array(eam_phis_filename, eam_use_pgrid)
 ! initialize global values that are used frequently
 call init_globals()
 
-! read the namelist &model_nml :: state_variables
-! to set up what will be read into the eam state vector
-call set_eam_variable_info(eam_template_filename, state_variables)
+! parse_variables converts the character table that was read in from
+! model_nml:state_variables and returns a state_var_type
+! that can be passed to add_domain
+domain_id = add_domain(eam_template_filename, parse_variables_clamp(state_variables))
+
+call verify_state_var_list
 
 ! The size of the only surface pressure dimension is the number of columns
 ncol_temp = get_dim_lengths(domain_id,  get_varid_from_kind(domain_id, QTY_SURFACE_PRESSURE))
@@ -1440,14 +1441,14 @@ do icorner=1, 4
    call get_se_values_from_nonstate_fields(state_handle, ens_size, four_corners(icorner), &
                               four_levs1(icorner, :), obs_qty, precise, vals1, my_status)
    if (any(my_status /= 0)) then
-      my_status(:) = 16   ! cannot retrieve vals1 values
+      my_status(:) = 26   ! cannot retrieve vals1 values
       return
    endif
 
    call get_se_values_from_nonstate_fields(state_handle,  ens_size, four_corners(icorner), &
                               four_levs2(icorner, :), obs_qty, precise, vals2, my_status)
    if (any(my_status /= 0)) then
-      my_status(:) = 17   ! cannot retrieve top values
+      my_status(:) = 27   ! cannot retrieve top values
       return
    endif
 
@@ -3369,6 +3370,37 @@ end subroutine solve_quadratic
 
 !-----------------------------------------------------------------------
 
+subroutine verify_state_var_list
+
+! No return is needed.  A diagnostic statement is printed before a potential
+! failure
+! of get_varid_from_varname.  Success means carry on.
+
+! There could be more moisture variables than the traditional 3.
+character(len=32), dimension(3) :: var_names
+integer:: i, varid
+
+data var_names /'Q', 'CLDLIQ', 'CLDICE'/
+
+! PS is required for both eam-se and eam-fv, 
+! and regardless of the dry mass logical variables status.
+varid = get_varid_from_varname(domain_id, 'PS')
+if (varid == -1) then
+   write(string1, *) 'PS needs to be among the state variables.'
+   call error_handler(E_ERR, 'verify_state_var_list', string1, source, revision, revdate)
+endif
+
+if (dry_mass_vertical_coordinate .and. precise_dry_mass_get_close) then
+   do i = 1,size(var_names)
+      varid = get_varid_from_varname(domain_id, var_names(i))
+      if (varid == -1) then
+         write(string1, *) trim(var_names(i)),' needs to be among the state variables for dry_mass.'
+         call error_handler(E_ERR, 'verify_state_var_list', string1, source, revision, revdate)
+      endif
+   enddo
+endif
+
+end subroutine verify_state_var_list
 
 !-----------------------------------------------------------------------------------------------------
 ! End of routines for computing horizontal grid box location with cubed sphere spectral element grids 
